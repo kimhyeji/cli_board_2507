5445d4a136fabcc47ce8052dc3f321d0a55b83ab
kimhyeji <gpwl5589@gamil.com>
2025-07-11 18:40:26 +0900

    4단계

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -7,19 +7,22 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("== 게시판 앱 ==");
 
+        int lastId = 1;
+
         while (true) {
             System.out.print("명령) ");
-            String command = sc.nextLine();
+            String command = sc.nextLine().trim();
 
             if ( command.equals("종료") ) {
                 break;
             } else if ( command.equals("등록") ) {
                 System.out.print("제목 : ");
-                String subject = sc.nextLine();
+                String subject = sc.nextLine().trim();
                 System.out.print("내용 : ");
-                String content = sc.nextLine();
+                String content = sc.nextLine().trim();
 
-                System.out.println("1번 게시글이 등록되었습니다.");
+                System.out.printf("%d번 게시글이 등록되었습니다.\n", lastId);
+                lastId++;
             }
         }
 
