6c1abdd18da18a8d8f2ee232a4af66de687314f2
kimhyeji <gpwl5589@gamil.com>
2025-07-14 15:11:48 +0900

    5단계 정렬 수정

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -38,7 +38,7 @@ public class App {
                 System.out.println("번호 / 제목 / 내용");
                 System.out.println("----------------------");
 
-                for (int i = 0; i < articleList.size(); i++) {
+                for (int i = articleList.size() - 1; i >= 0; i--) {
                     Article article = articleList.get(i);
                     System.out.printf("%d / %s / %s\n", article.getId(), article.getSubject(), article.getContent());
                 }
